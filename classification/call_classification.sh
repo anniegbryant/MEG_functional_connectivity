2240cdb1d9702589af77d57ac71fb979b0b5394a
anniegbryant <anniegbryant@gmail.com>
2024-07-17 02:40:02 +0900

    feature extraction and classification starter code

---
+++ classification/call_classification.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Define the batch job array command
+input_model_file=/project/hctsa/annie/github/MEG_functional_connectivity/subject_list_Cogitate_MEG.txt
+
+###################### Averaged epoch classification ##################
+n_jobs=10
+cmd="qsub -o /project/hctsa/annie/github/MEG_functional_connectivity/cluster_output/Cogitate_MEG_group_averaged_classification.out \
+   -N MEG_averaged_classification \
+   -l select=1:ncpus=$n_jobs:mem=20GB:mpiprocs=$n_jobs \
+   -v input_model_file=$input_model_file,n_jobs=$n_jobs \
+   run_averaged_classifiers.pbs"
+$cmd
+
+###################### Indivivdual epoch classification ##################
+# n_jobs=10
+# cmd="qsub -o /project/hctsa/annie/github/MEG_functional_connectivity/cluster_output/Cogitate_MEG_individual_epoch_classification_^array_index^.out \
+#    -N MEG_individual_classification \
+#    -J 1-100 \
+#    -l select=1:ncpus=$n_jobs:mem=20GB:mpiprocs=$n_jobs \
+#    -v input_model_file=$input_model_file,n_jobs=$n_jobs \
+#    run_individual_classifiers.pbs"
+# $cmd
---
+++ feature_extraction/call_feature_extraction.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# User-define batch number
+batch_number=$1
+
+# Define the batch job array command
+# input_model_file=/project/hctsa/annie/github/MEG_functional_connectivity/subject_list_Cogitate_MEG.txt
+input_model_file=/headnode1/abry4213/github/MEG_functional_connectivity/subject_list_Cogitate_MEG.txt
+
+##################################################################################################
+# Running pyspi across subjects, averaged epochs
+##################################################################################################
+
+# # Averaged epochs
+# cmd="qsub -o /headnode1/abry4213/github/Cogitate_Connectivity_2024/cluster_output/Cogitate_Batch${batch_number}_pyspi_averaged_^array_index^_fast.out \
+#    -J 1-100 \
+#    -N fast_pyspi_MEG \
+#    -l select=1:ncpus=1:mem=20GB:mpiprocs=1 \
+#    -v input_model_file=$input_model_file,batch_number=$batch_number \
+#    run_pyspi_for_subject_averaged_epochs.pbs"
+# $cmd
+
+##################################################################################################
+# Running pyspi across subjects, individual epochs
+##################################################################################################
+
+# Individual epochs
+cmd="qsub -o /headnode1/abry4213/github/Cogitate_Connectivity_2024/cluster_output/pyspi_for_individual_epochs_^array_index^_fast.out \
+   -N pyspi_individual_epochs \
+   -J 1-100 \
+   -l select=1:ncpus=1:mem=20GB:mpiprocs=1 \
+   -v input_model_file=$input_model_file,batch_number=$batch_number \
+   run_pyspi_for_subject_individual_epochs.pbs"
+$cmd
